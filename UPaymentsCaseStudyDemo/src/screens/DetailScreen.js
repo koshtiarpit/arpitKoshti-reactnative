486f76e26f2fda2ced38bfeb6ac31c3ec4ed38b6
koshtiarpit <arpit.koshti94284@gmail.com>
2022-08-28 00:49:02 +0530

    completed home and detail screen

--- UPaymentsCaseStudyDemo/src/screens/DetailScreen.js
+++ UPaymentsCaseStudyDemo/src/screens/DetailScreen.js
@@ -1,13 +1,110 @@
+import axios from 'axios';
 import React, {Component} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {Dimensions, Text, TouchableOpacity, View, Image, ActivityIndicator} from 'react-native';
+import {access_token} from '../Constants';
 
 class DetailScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      product: {},
+    };
+  }
+
+  componentDidMount() {
+    this.goForAxiosProduct(this.props.route.params.item._id);
+  }
+
+  goForAxiosProduct = _id => {
+    this.setState({
+      loading: true,
+    });
+    axios
+      .get(
+        `https://upayments-studycase-api.herokuapp.com/api/products/${_id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        },
+      )
+      .then(response => {
+        // console.log(
+        //   'getting products details from axios',
+        //   JSON.stringify(response.data.product, null, 2),
+        // );
+        setTimeout(() => {
+          this.setState({
+            loading: false,
+            product: response.data.product,
+          });
+        }, 2000);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
+    if (this.state.loading) {
+      return <ActivityIndicator color={'black'} size={32} style={{flex: 1}} />;
+    }
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <TouchableOpacity onPress={() => {}}>
-          <Text>Detail Screen</Text>
-        </TouchableOpacity>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        <Image
+          resizeMode="contain"
+          onError={() => this.goForAxiosProducts()}
+          style={{
+            width: '90%',
+            height: Dimensions.get('screen').width * 0.9,
+            alignSelf: 'center',
+            marginTop: '5%',
+          }}
+          source={{uri: this.state.product.avatar}}
+        />
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            borderWidth: 1,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            padding: '4%',
+            paddingTop: '3%',
+            backgroundColor: 'black',
+          }}
+          onPress={() => {}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              marginVertical: '5%',
+              justifyContent: 'space-between',
+            }}>
+            <Text
+              style={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>{`${
+              this.state.product.name || ''
+            }`}</Text>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 20,
+                fontWeight: 'bold',
+              }}>
+              {this.state.product.price ? `$${this.state.product.price}` : ''}
+            </Text>
+          </View>
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 12,
+              textAlign: 'justify',
+              marginTop: '3%',
+              marginBottom: '10%',
+            }}>{`${this.state.product.description || ''}`}</Text>
+        </View>
       </View>
     );
   }
--- UPaymentsCaseStudyDemo/src/screens/HomeScreen.js
+++ UPaymentsCaseStudyDemo/src/screens/HomeScreen.js
@@ -1,16 +1,184 @@
 import React, {Component} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {FlatList, Text, TouchableOpacity, View, Image, ActivityIndicator} from 'react-native';
 import {FloatingAction} from 'react-native-floating-action';
 import Icon from 'react-native-vector-icons/Ionicons';
+import axios from 'axios';
+import {access_token} from '../Constants';
 
 class HomeScreen extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      categories: [],
+      products: [],
+      selectedCategory: 'All',
+    };
+  }
+
+  componentDidMount() {
+    this.goForAxiosCategories();
+    this.goForAxiosProducts();
+  }
+
+  goForAxiosCategories = () => {
+    this.setState({
+      loading: true,
+    });
+    axios
+      .get('https://upayments-studycase-api.herokuapp.com/api/categories/', {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+      .then(response => {
+        // console.log(
+        //   'getting categories data from axios',
+        //   JSON.stringify(response.data.categories, null, 2),
+        // );
+        setTimeout(() => {
+          this.setState({
+            loading: false,
+            categories: [{name: 'All'}, ...response.data.categories],
+          });
+        }, 2000);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  goForAxiosProducts = () => {
+    this.setState({
+      loading: true,
+    });
+    axios
+      .get('https://upayments-studycase-api.herokuapp.com/api/products', {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+      .then(response => {
+        // console.log(
+        //   'getting products data from axios',
+        //   JSON.stringify(response.data.categories, null, 2),
+        // );
+        setTimeout(() => {
+          this.setState({
+            loading: false,
+            products: response.data.products,
+          });
+        }, 2000);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  renderCategoryItem = name => {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View
+        style={{
+          borderWidth: 2,
+          justifyContent: 'center',
+          marginHorizontal: 5,
+          borderRadius: 10,
+          backgroundColor:
+            this.state.selectedCategory === name ? 'white' : 'black',
+        }}>
         <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('Detail')}>
-          <Text>Home Screen</Text>
+          onPress={() => this.setState({selectedCategory: name})}>
+          <Text
+            style={{
+              fontSize: 14,
+              color: this.state.selectedCategory === name ? 'black' : 'white',
+              marginHorizontal: 10,
+            }}>
+            {name}
+          </Text>
         </TouchableOpacity>
+      </View>
+    );
+  };
+
+  renderProductItem = item => {
+    return (
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate('Detail', {item})}
+        style={{
+          borderWidth: 1,
+          borderRadius: 5,
+          marginBottom: '2%',
+          marginRight: '2%',
+          width: '48%',
+          justifyContent: 'center',
+        }}>
+        <Image
+          resizeMode="contain"
+          onError={() => this.goForAxiosProducts()}
+          style={{width: 100, height: 100, alignSelf: 'center', margin: '5%'}}
+          source={{uri: item.avatar}}
+        />
+        <View style={{backgroundColor: 'black', padding: 5, borderRadius: 5}}>
+          <Text style={{flex: 1, color: 'white'}}>{item.name}</Text>
+          <Text style={{flex: 1, color: 'white'}}>{`$${item.price}`}</Text>
+          <Image
+            resizeMode="contain"
+            onError={() => this.goForAxiosProducts()}
+            style={{
+              position: 'absolute',
+              width: 30,
+              height: 25,
+              bottom: 10,
+              right: 10,
+              // margin: '5%',
+            }}
+            source={require('../assets/pencil.png')}
+          />
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  render() {
+    if (this.state.loading) {
+      return <ActivityIndicator color={'black'} size={32} style={{flex: 1}} />;
+    }
+    return (
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        <FlatList
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          style={{
+            flexGrow: 0,
+            height: '10%',
+          }}
+          contentContainerStyle={{
+            padding: 10,
+            paddingVertical: 15,
+          }}
+          data={this.state.categories}
+          renderItem={({item}) => this.renderCategoryItem(item.name)}
+        />
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          style={{
+            flexGrow: 0,
+          }}
+          contentContainerStyle={{
+            paddingVertical: 5,
+            paddingLeft: '2%',
+          }}
+          numColumns={2}
+          data={
+            this.state.selectedCategory === 'All'
+              ? this.state.products
+              : this.state.products.filter(
+                  item => item.category === this.state.selectedCategory,
+                )
+          }
+          renderItem={({item}) => this.renderProductItem(item)}
+        />
         <FloatingAction
           ref={ref => {
             this.floatingAction = ref;
